b4b00935193f1d49d732bc8f178b1c4761c60380
Aditya Thakral <aditya.1998thakral@gmail.com>
2015-02-03 22:09:33 +0530

    Departments added, Sub-depts added
    New function, lonely()

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 username=""
 password=""
+i=0
 
 class db():
-    username=["aditya","arsh","swapnil"]
-    password=["thakral","rampal","satpute"]
-    name=["Aditya Thakral","Arsh Rampal","Swapnil Satpute"]
-    email=["","",""]
-    phone=["","",""]
-    dep=["admin","admin","admin"]
+    username=["thk98","arsh","swapnil","jatin"]
+    password=["thakral","rampal","satpute","sethi"]
+    name=["Aditya Thakral","Arsh Rampal","Swapnil Satpute","Jatin Sethi"]
+    email=["aditya.1998thakral@gmail.com","","",""]
+    phone=["9717552963","","",""]
+    dep=["it","it","it","Finance"]
+    pos=["Head","Head","Head"]
     lastlogin=["","",""]
     lastloc=["","",""]
     
@@ -16,10 +18,16 @@ def wait():
     x=raw_input()
     return None
 
+def lonely():
+    print """
+You are in Human Resources.
+It's lonely here. We'll add someone here. Don't worry."""
+
 def login():
     print "Please login to continue."
     global username
     global password
+    global i
     username=raw_input("Username: ")
     password=raw_input("Password: ")
     if username in db.username:
@@ -37,8 +45,8 @@ def login():
         print "Incorrect username or/and password."
 
 def menu():
-    print "Where do you want to go"
-    choice=raw_input("""
+    print "Where do you want to go?"
+    choice=int(raw_input("""
 1. Human Resources
 2. Administrative Office
 3. Marketing
@@ -46,7 +54,81 @@ def menu():
 5. Assembly Line
 6. Canteen
 7. Washroom
-8. IT office\n""")
- 
+8. IT Lab\nEnter a number corresponding to your choice"""))
+    if choice==1:
+        hr()
+    elif choice==2:
+        admin()
+    elif choice==3:
+        market()
+    elif choice==4:
+        finance()
+    elif choice==5:
+        assl()
+    elif choice==6:
+        cant()
+    elif choice==7:
+        wash()
+    elif choice==8:
+        it()
+    else:
+        print "Enter a valid choice!"
+        menu()
+
+
+
+def hr():
+    lonely()
+    """
+recuitment
+employee welfare
+salary
+PF
+Labour Laws
+admin
+    """
+
+def admin():
+    lonely()
+#remove admin
+def market():
+    lonely()
+    """
+product identification
+market requirements
+market location
+category of market
+    """
+
+def finance():
+    lonely()
+    """
+Management of cash flow
+Management of expenses
+Management of income
+Management of Profitablity
+Investments
+    """
+def assl():
+    lonely()
+    """
+Components
+Jig and Fixture
+Testing Equipments
+Assembly Lines
+    """
+
+def cant():
+    lonely()
+
+def wash():
+    lonely()
+
+def it():
+    print "WIP"
+    """
+Server room-A/C
+Database
+Computers
+    """
     
-        
